31c70d24735335fc171d37d75d41b38fbaf7b91a
lakshmana <lakshman.chimata@gmail.com>
2017-02-16 18:39:16 +0530

    added little animations for actuator brightness

--- src/app/electrician/configdetector/cdetectore.component.ts
+++ src/app/electrician/configdetector/cdetectore.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy} from '@angular/core';
+import { Component ,trigger, state, animate, transition, style, OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy,Renderer, ViewChild, ElementRef} from '@angular/core';
 import { LoggerService } from '../../logger.service';
 import { DataService } from '../../data.service';
 import { RouterModule, Routes ,Router,RouterStateSnapshot,ActivatedRoute} from '@angular/router';
@@ -7,7 +7,23 @@ import { RouterModule, Routes ,Router,RouterStateSnapshot,ActivatedRoute} from '
 @Component({
   selector: 'cdetectore-root',
   templateUrl: './cdetectore.component.html',
-  styleUrls: ['./cdetectore.component.css']
+  styleUrls: ['./cdetectore.component.css'],
+  animations: [
+      trigger('slideIn', [
+        state('*', style({
+            transform: 'translateX(+100%)',
+        })),
+        state('in', style({
+            width:'300px',
+            transform: 'translateX(0%)',
+        })),
+        state('out',   style({
+            transform: 'translateX(+100%)',
+        })),
+        transition('* => in', animate('500ms ease-in')),
+        transition('in => out', animate('500ms ease-in')),
+        ])
+    ]
 })
 export class CDetectorEComponent implements OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy{
 
@@ -17,11 +33,19 @@ export class CDetectorEComponent implements OnChanges,OnInit ,DoCheck,AfterConte
     onLabel = 'on';
     offLable = 'off';
     deviceType = -1;
-    constructor(private logger: LoggerService,private data: DataService, private router:Router,private route:ActivatedRoute) {
+    brthresholderror = false;
+    brsetpointerror = false;
+    aslider = 'none';
+    showSlider = false;
+    constructor(private logger: LoggerService,private data: DataService, 
+                  private router:Router,private route:ActivatedRoute,
+                private renderer:Renderer,private elRef:ElementRef) {
       this.activeDevice = this.data.getSelectedDevice();
       this.ad = this.data.getDevicedata();
       this.deviceType = data.getSelectedDeviceType();
       this.data.setFooter(true);
+      this.aslider = 'none';
+      this.showSlider = false;
     }
 
   ngOnChanges() { 
@@ -43,11 +67,71 @@ export class CDetectorEComponent implements OnChanges,OnInit ,DoCheck,AfterConte
   gotoaddParams(){
     this.router.navigate(['addparams'],{relativeTo: this.route});
   }
-  
+
+  reduceBrightness(item) {
+    if(item == 'threshold')
+      this.ad.sensor_settings.brightness_threshold = this.ad.sensor_settings.brightness_threshold - 1;
+    else if(item == 'setpoint')
+          this.ad.sensor_settings.brightness_setpoint = this.ad.sensor_settings.brightness_setpoint - 1;
+    this.validatebrparams(item)
+  }
+
+  increaseBrightness(item) {
+    if(item == 'threshold')
+      this.ad.sensor_settings.brightness_threshold = this.ad.sensor_settings.brightness_threshold + 1;
+    else if(item == 'setpoint')
+          this.ad.sensor_settings.brightness_setpoint = this.ad.sensor_settings.brightness_setpoint + 1;
+    this.validatebrparams(item)  
+    }
+
+
+  validatebrparams(item) {
+    if(item == 'threshold') {
+      if(this.ad.sensor_settings.brightness_threshold < 10 || 
+      this.ad.sensor_settings.brightness_threshold >2000) {
+        this.brthresholderror =  true;
+      }else {
+        this.brthresholderror =  false;
+      }
+    }else if(item == 'setpoint') {
+      if(this.ad.sensor_settings.brightness_setpoint < 10 || 
+      this.ad.sensor_settings.brightness_setpoint >2000) {
+        this.brsetpointerror =  true;
+      }else {
+        this.brsetpointerror =  false;
+      }
+    }
+  }
+  slideBrightnessIn() {
+    this.aslider = 'in';
+  }
+  slideBrightnessOut() {
+    this.aslider = 'out';
+  }
+  setActuatorBrightness() {
+  }
+slideBackground (value) {
+  let stringval = value.toString();
+  let stylestr = "linear-gradient(to right,#2c435c " + stringval + "%, transparent 0%";
+  let mystyles =  {
+     'background': stylestr
+  }
+  return mystyles;
+  }
+  animationStarted($event) {
+     if($event.toState == "in") {
+        this.showSlider = true;
+    }
+  }
+  animationDone($event) {
+    if($event.toState == "out") {
+        this.showSlider = false;
+    }
+  }
+
   ngOnDestroy() {
   }
   deviceNameChanged(nameChanged) {
-    this.logger.log('name changed to ' + nameChanged);
   }
   gotoActuator1(){
     this.router.navigate(['eactuator1'],{relativeTo: this.route});
