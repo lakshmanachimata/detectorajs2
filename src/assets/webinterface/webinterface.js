39bb929c84901ea89744743dbedda87473915116
lakshmana <lakshman.chimata@gmail.com>
2017-03-02 16:40:45 +0530

    webinterface js added for communicating to and fro native

---
+++ src/assets/webinterface/webinterface.js
@@ -0,0 +1,11 @@
+
+function BJ_showToast(toast) {
+    BJE.showToast(toast);
+}
+function BJ_startBLEScan(toast) {
+    BJE.startBLEScan(toast);
+}
+function BJ_stopBLEScan(toast) {
+    BJE.stopBLEScan(toast);
+}
+
