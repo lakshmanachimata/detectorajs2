fa9310fdfc5eac76a929859c4d5cc3212598fefd
lakshmana <lakshman.chimata@gmail.com>
2017-03-21 19:49:44 +0530

    added other params writing params

--- src/app/electrician/configdetector/eactuator2/eactuator2.component.ts
+++ src/app/electrician/configdetector/eactuator2/eactuator2.component.ts
@@ -2,7 +2,8 @@ import { Component , OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChec
 import { LoggerService } from '../../../logger.service';
 import { DataService } from '../../../data.service';
 import { RouterModule, Routes ,Router,RouterStateSnapshot} from '@angular/router';
-
+import {SCCP_DATATYPES} from'../../../data.service';
+import {SCCP_ATTRIBUTES} from'../../../data.service';
 
 @Component({
   selector: 'eactuator2-root',
@@ -40,19 +41,34 @@ export class EActuator2Component implements OnChanges,OnInit ,DoCheck,AfterConte
   reduceCount(item){
     if(item == 'sondelay') {
       this.ad.actuator2.durable_on_off_switching.duration_on = this.ad.actuator2.durable_on_off_switching.duration_on -1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.HVAC_SWITCH_ON_DELAY,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.actuator2.durable_on_off_switching.duration_on])
     }else if(item == 'soffdelay') {
       this.ad.actuator2.durable_on_off_switching.duration_off = this.ad.actuator2.durable_on_off_switching.duration_off -1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.HVAC_SWITCH_OFF_DELAY,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.actuator2.durable_on_off_switching.duration_off])
     }
 
   }
   increaseCount(item) {
  if(item == 'sondelay') {
       this.ad.actuator2.durable_on_off_switching.duration_on = this.ad.actuator2.durable_on_off_switching.duration_on + 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.HVAC_SWITCH_ON_DELAY,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.actuator2.durable_on_off_switching.duration_on])
     }else if(item == 'soffdelay') {
       this.ad.actuator2.durable_on_off_switching.duration_off = this.ad.actuator2.durable_on_off_switching.duration_off + 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.HVAC_SWITCH_OFF_DELAY,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.actuator2.durable_on_off_switching.duration_off])
     }
   }
   onBLEdata() {
     
   }
+  circuitModeChange() {
+    this.data.addToSendData([SCCP_ATTRIBUTES.CH2_CIRCUIT_LOGIC,SCCP_DATATYPES.SCCP_TYPE_ENUM8,this.ad.actuator2.circuit_logic])
+  }
+  ch2ModeChange(){
+    this.data.addToSendData([SCCP_ATTRIBUTES.CH2_MODE,SCCP_DATATYPES.SCCP_TYPE_ENUM8,this.ad.actuator2.ch2_mode])
+  }
+  togglehvacDC(){
+    this.ad.actuator2.hvacDynamicalControlEnable = !this.ad.actuator2.hvacDynamicalControlEnable
+    this.data.addToSendData([SCCP_ATTRIBUTES.HVAC_DYNAMICAL_CONTROL_ENABLE,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.actuator2.hvacDynamicalControlEnable?1:0])
+  }
+
 }
--- src/app/electrician/configdetector/otherparams/otherparams.component.ts
+++ src/app/electrician/configdetector/otherparams/otherparams.component.ts
@@ -2,7 +2,8 @@ import { Component , OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChec
 import { LoggerService } from '../../../logger.service';
 import { DataService } from '../../../data.service';
 import { RouterModule, Routes ,Router,RouterStateSnapshot,ActivatedRoute} from '@angular/router';
-
+import {SCCP_DATATYPES} from'../../../data.service';
+import {SCCP_ATTRIBUTES} from'../../../data.service';
 
 @Component({
   selector: 'other-root',
@@ -17,7 +18,6 @@ export class EOtherParamsComponent implements OnChanges,OnInit ,DoCheck,AfterCon
   offLabel = 'off';
   showFitterPin = false;
   showUserPin = false;
-  DeactivateOp = false;
   A1LoadIdentify = false;
   A2LoadIdentify = false;
   masterQuad = 'q1';
@@ -51,8 +51,10 @@ export class EOtherParamsComponent implements OnChanges,OnInit ,DoCheck,AfterCon
   reduceCount(item) {
     if(item == 'cload') {
       this.ad.service.energy_monitor.connected_load = this.ad.service.energy_monitor.connected_load - 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.ENERGY_MONITOR_CONNECTED_LOAD,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.service.energy_monitor.connected_load])
     }else if(item == 'lduration') {
       this.ad.service.energy_monitor.lighting_duration_per_week = this.ad.service.energy_monitor.lighting_duration_per_week - 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.ENERGY_MONITOR_LIGHTING_DURATION,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.service.energy_monitor.lighting_duration_per_week])
     }else if(item == 'eprice') {
       this.ad.service.energy_monitor.electricity_price = this.ad.service.energy_monitor.electricity_price - 1;
     }
@@ -61,13 +63,53 @@ export class EOtherParamsComponent implements OnChanges,OnInit ,DoCheck,AfterCon
   increaseCount(item) {
     if(item == 'cload') {
       this.ad.service.energy_monitor.connected_load = this.ad.service.energy_monitor.connected_load + 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.ENERGY_MONITOR_CONNECTED_LOAD,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.service.energy_monitor.connected_load])
     }else if(item == 'lduration') {
       this.ad.service.energy_monitor.lighting_duration_per_week = this.ad.service.energy_monitor.lighting_duration_per_week + 1;
+      this.data.addToSendData([SCCP_ATTRIBUTES.ENERGY_MONITOR_LIGHTING_DURATION,SCCP_DATATYPES.SCCP_TYPE_UINT16,this.ad.service.energy_monitor.lighting_duration_per_week])
     }else if(item == 'eprice') {
       this.ad.service.energy_monitor.electricity_price = this.ad.service.energy_monitor.electricity_price + 1;
     }
   }
   onBLEdata() {
     
+  }
+  toggleDP(){
+    this.ad.service.testmode.deactivate_outputs_enable = !this.ad.service.testmode.deactivate_outputs_enable;
+    this.data.addToSendData([SCCP_ATTRIBUTES.TEST_MODE_DEACTIVATE_OUTPUTS_ENABLE,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.service.testmode.deactivate_outputs_enable])
+  }
+  CNameChanged() {
+    this.data.addToSendData([SCCP_ATTRIBUTES.CONTACT,SCCP_DATATYPES.SCCP_TYPE_STRING,this.activeDevice.contactName])
+  }
+  BuildingChanged() {
+    this.data.addToSendData([SCCP_ATTRIBUTES.BUILDING,SCCP_DATATYPES.SCCP_TYPE_STRING,this.activeDevice.buildingName])
+  }
+  togglepbr(){
+    this.ad.commissioning.user_settings.brightness_threshold = !this.ad.commissioning.user_settings.brightness_threshold
+    this.data.addToSendData([SCCP_ATTRIBUTES.ENABLE_USER_SET_BRIGHTNESS_THRESHOLD,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.commissioning.user_settings.brightness_threshold])
+  }
+  togglessod(){
+    this.ad.commissioning.user_settings.switch_off_delay = !this.ad.commissioning.user_settings.switch_off_delay
+        this.data.addToSendData([SCCP_ATTRIBUTES.ENABLE_USER_SET_SWITCH_OFF_DELAY,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.commissioning.user_settings.switch_off_delay])
+  }
+  toggleem(){
+    this.ad.commissioning.user_settings.energy_monitor = !this.ad.commissioning.user_settings.energy_monitor
+    this.data.addToSendData([SCCP_ATTRIBUTES.ENABLE_USER_ENERGY_MONITOR,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.commissioning.user_settings.energy_monitor])
+  }
+  togglebi(){
+    this.ad.commissioning.user_settings.basic_illumination = !this.ad.commissioning.user_settings.basic_illumination
+    this.data.addToSendData([SCCP_ATTRIBUTES.ENABLE_USER_BASIC_BRIGHTNESS,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.commissioning.user_settings.basic_illumination])
+  }
+  togglenf(){
+    this.ad.commissioning.user_settings.anti_glare_function = !this.ad.commissioning.user_settings.anti_glare_function
+    this.data.addToSendData([SCCP_ATTRIBUTES.ENABLE_USER_NIGHT_LIGHT_FUNCTION,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.commissioning.user_settings.anti_glare_function])
+  }
+  togglech1ID(){
+    this.ad.service.testmode.ch1_identify_load = this.ad.service.testmode.ch1_identify_load;
+    this.data.addToSendData([SCCP_ATTRIBUTES.CH1_IDENTIFYING_LOAD,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.service.testmode.ch1_identify_load ])
+  }
+  togglech2ID(){
+    this.ad.service.testmode.ch2_identify_load = this.ad.service.testmode.ch2_identify_load;
+    this.data.addToSendData([SCCP_ATTRIBUTES.CH2_IDENTIFYING_LOAD,SCCP_DATATYPES.SCCP_TYPE_BOOL,this.ad.service.testmode.ch2_identify_load ])
   }
 }
