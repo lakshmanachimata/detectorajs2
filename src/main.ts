37a30750c80eb7b0b04329213c396a7b2ead17de
lakshmana <lakshman.chimata@gmail.com>
2017-05-25 14:48:36 +0530

    error solved

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import './polyfills.ts';
-
+import { enableProdMode,TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID,Provider,PlatformRef ,Injectable} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { i18n_Lang_Defs } from  './app/i18n-data';
 import { AppModule } from './app/app.module';
+
+
 
 if (environment.production) {
   enableProdMode();
 }
+let langs:i18n_Lang_Defs = new i18n_Lang_Defs();
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//let appRef:PlatformRef =platformBrowserDynamic();
+//platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(
+         AppModule,
+         {
+            providers: 
+            [
+              {provide: TRANSLATIONS, useValue: langs.getTranslations()},
+              {provide:TRANSLATIONS_FORMAT, useValue:'xlf'},
+              {provide:LOCALE_ID, useValue:langs.userLanguageCode}
+            ],
+        }
+         );
